5edee6c61e505caa426f5cabf73cbea4f33a49d1
patychavezromo <patricia.araceli.chavez.romo@gmail.com>
2021-11-07 17:42:00 -0600

    Commit inicial

---
+++ src/Sumador5Numeros.java
@@ -0,0 +1,19 @@
+
+
+public class Sumador5Numeros {
+
+    public static void main(String[] args){
+        int contador=0;
+        int suma=0;
+
+
+        while(contador<=5) {
+
+            suma = suma + contador;
+            contador++;
+        }
+            System.out.println("la suma total es " + suma);
+
+}
+
+}
